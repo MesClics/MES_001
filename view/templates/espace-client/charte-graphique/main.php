2bbf01f1691128efce0b5f61ba7b66b23f6eb249
MesClics <mesclics@hotmail.com>
2017-10-02 17:51:20 +0200

    styles menu espace client

--- view/templates/espace-client/charte-graphique/main.php
+++ view/templates/espace-client/charte-graphique/main.php
@@ -4,3 +4,10 @@
     <p>Pour une communication efficace, il est conseillé de toujours utiliser les mêmes codes graphiques. Cela permet de ne pas « brouiller » l’information auprès de vos clients et de les fidéliser à votre « marque ».</p>
     <p>Voici la liste des codes graphiques à respecter pour être fidèle à la charte graphique de votre site internet.</p>
 </div>
+
+<nav>
+    <ul>
+        <li><a href="<?php echo $home_url.$folder_name;?>/">Mon espace client</a></li>
+        <li><a href="<?php echo $home_url.$folder_name;?>/contrat/">Mon contrat</a></li>
+    </ul>
+</nav>
